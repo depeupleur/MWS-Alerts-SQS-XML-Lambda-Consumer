f38695cc032ca7225b0df163fe96fc31c34a53cc
Juan Murillo <depeupleur@gmail.com>
2018-07-22 16:05:59 -0600

    removing tabs which were causing incorrect indentation in XMLParser

--- XMLParser.py
+++ XMLParser.py
@@ -18,14 +18,14 @@ MWSQUEUE = os.environ.get('MWSQUEUE')
 MWSMAXMSG =  int(os.environ.get('MWSMAXMSG'))
 
 def lambda_handler(event, context):
-	
-	# uncomment to output function event and context to logs
+
+    # uncomment to output function event and context to logs
     #if context:
     #    LOGGER.debug('Function ARN: %s', context.invoked_function_arn)
     #else:
     #    LOGGER.warning('Lambda context is missing')    
     LOGGER.info(json.dumps(event))
-	
+
     # create  sqs service resource 
     sqs = boto3.resource('sqs')
     
@@ -35,17 +35,17 @@ def lambda_handler(event, context):
     # parse xml messages and print xml and json versions to log
     for message in queue.receive_messages(MaxNumberOfMessages=MWSMAXMSG):
         
-		#parse xml string to json and then to string
-		msgdict = xmltodict.parse(message.body)
-		dictdump = json.dumps(msgdict)
+        #parse xml string to json and then to string
+        msgdict = xmltodict.parse(message.body)
+        dictdump = json.dumps(msgdict)
         
-		#output data to log
-		LOGGER.info('__________________________________________')
-		LOGGER.info('Message ID:')
-		LOGGER.info(message.message_id)
-		LOGGER.info('Message Body in XML:')
-		LOGGER.info(message.body)
-		LOGGER.info('Message Body as a dict:')
+        #output data to log
+        LOGGER.info('__________________________________________')
+        LOGGER.info('Message ID:')
+        LOGGER.info(message.message_id)
+        LOGGER.info('Message Body in XML:')
+        LOGGER.info(message.body)
+        LOGGER.info('Message Body as a dict:')
         LOGGER.info(dictdump)
         LOGGER.info('__________________________________________')
     
